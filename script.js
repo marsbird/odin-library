ca66b1b43ef4f859abfa321b7c4bc91cfaf0b8c2
Marselena <github.seaplanes@simplelogin.co>
2023-01-27 21:49:46 -0500

    Add length to book object

--- script.js
+++ script.js
@@ -2,20 +2,22 @@ const library = [];
 const div = document.querySelector("div.grid-container");
 const btn = document.querySelector("button");
 
-function Book(title, author) {
+function Book(title, author, length) {
   this.title = title;
   this.author = author;
+  this.length = length;
 }
 
-function addBookToLibrary(title, author) {
+function addBookToLibrary(title, author, length) {
   // create new book object and add to array
-  const book = new Book(title, author);
+  const book = new Book(title, author, length);
   library.push(book);
 
   //   create new DOM elements
   const cardElement = document.createElement("div");
   const titleElement = document.createElement("p");
   const authorElement = document.createElement("p");
+  const lengthElement = document.createElement("p");
 
   // add class to card
   cardElement.classList = "card";
@@ -23,10 +25,12 @@ function addBookToLibrary(title, author) {
   // fill in text content
   titleElement.textContent = book.title;
   authorElement.textContent = book.author;
+  lengthElement.textContent = book.length;
 
   // add new elements to DOM
   cardElement.appendChild(titleElement);
   cardElement.appendChild(authorElement);
+  cardElement.appendChild(lengthElement);
   div.appendChild(cardElement);
 }
 
@@ -34,11 +38,17 @@ function addBookToLibrary(title, author) {
 btn.addEventListener("click", () => {
   const title = document.getElementById("title");
   const author = document.getElementById("author");
-  addBookToLibrary(title.value, author.value);
+  const length = document.getElementById("length");
+  addBookToLibrary(title.value, author.value, length.value);
   title.value = "";
   author.value = "";
+  length.value = "";
 });
 
-// add defaults and display in the DOM
-addBookToLibrary("A Game of Thrones", "George R. R. Martin");
-addBookToLibrary("The Final Empire", "Brandon Sanderson");
+// add defaults
+addBookToLibrary("A Game of Thrones", "George R. R. Martin", "819 pages");
+addBookToLibrary(
+  "Mistborn: The Final Empire",
+  "Brandon Sanderson",
+  "676 pages"
+);
