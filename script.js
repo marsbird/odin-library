62182e0a0edc1ca1cb6b7c7ec9becf4d27cdaee2
Marselena <github.seaplanes@simplelogin.co>
2023-02-02 18:01:57 -0500

    Add button to remove book from library

--- script.js
+++ script.js
@@ -8,30 +8,49 @@ function Book(title, author, length) {
   this.length = length;
 }
 
+function removeBookFromLibrary(indexNum) {
+  const cardElement = document.querySelector(`[data-index="${indexNum}"]`);
+  // remove book from library array
+  library.splice(indexNum, 1);
+  // remove book from document
+  cardElement.remove();
+}
+
 function addBookToLibrary(title, author, length) {
   // create new book object and add to array
   const book = new Book(title, author, length);
   library.push(book);
+  const bookIndex = library.length - 1;
 
   //   create new DOM elements
   const cardElement = document.createElement("div");
   const titleElement = document.createElement("p");
   const authorElement = document.createElement("p");
   const lengthElement = document.createElement("p");
+  const removeButton = document.createElement("button");
 
-  // add class to card
+  // add attributes
   cardElement.classList = "card";
+  cardElement.dataset.index = bookIndex;
+  removeButton.classList = "removeBtn";
 
-  // fill in text content
+  // add text content
   titleElement.textContent = book.title;
   authorElement.textContent = book.author;
   lengthElement.textContent = book.length;
+  removeButton.textContent = "Remove Book";
 
   // add new elements to DOM
   cardElement.appendChild(titleElement);
   cardElement.appendChild(authorElement);
   cardElement.appendChild(lengthElement);
+  cardElement.appendChild(removeButton);
   div.appendChild(cardElement);
+
+  // remove book on button click
+  removeButton.addEventListener("click", () => {
+    removeBookFromLibrary(bookIndex);
+  });
 }
 
 // get user input values and send to book functions
